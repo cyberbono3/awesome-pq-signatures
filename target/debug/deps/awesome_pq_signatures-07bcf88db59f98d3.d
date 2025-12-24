c05d6896125f40f893c54cdb13929f7f85f2c269
Andrei Ivasko <cyberbono3@gmail.com>
2025-12-24 12:15:05 +0300

    .gitignore

--- target/debug/deps/awesome_pq_signatures-07bcf88db59f98d3.d
+++
@@ -1,5 +0,0 @@
-/Users/ai/go/src/github.com/cyberbono3/awesome-pq-signatures/target/debug/deps/libawesome_pq_signatures-07bcf88db59f98d3.rmeta: src/lib.rs
-
-/Users/ai/go/src/github.com/cyberbono3/awesome-pq-signatures/target/debug/deps/awesome_pq_signatures-07bcf88db59f98d3.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/awesome_pq_signatures-ec38fae446b519e4.d
+++
@@ -1,9 +0,0 @@
-/Users/ai/go/src/github.com/cyberbono3/awesome-pq-signatures/target/debug/deps/libawesome_pq_signatures-ec38fae446b519e4.rmeta: src/lib.rs Cargo.toml
-
-/Users/ai/go/src/github.com/cyberbono3/awesome-pq-signatures/target/debug/deps/awesome_pq_signatures-ec38fae446b519e4.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
