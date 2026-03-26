0b54e885e00b7355de4ac4f52579ae0386d493cd
Andrei Ivasko <cyberbono3@gmail.com>
2026-03-26 22:12:06 +0300

    wip

--- crates/less/build.rs
+++ crates/less/build.rs
@@ -1,41 +1,32 @@
-use std::env;
-use std::path::PathBuf;
+mod native_cc {
+    include!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/../../build_support/native_cc.rs"
+    ));
+}
 
 fn main() {
-    let crate_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let vendor_dir = crate_dir.join("vendor/neon");
-    let include_dir = vendor_dir.join("include");
-    let lib_dir = vendor_dir.join("lib");
-    let native_dir = crate_dir.join("native");
-
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed={}", include_dir.display());
-    println!("cargo:rerun-if-changed={}", lib_dir.display());
-    println!("cargo:rerun-if-changed={}", native_dir.display());
-
-    cc::Build::new()
-        .include(&include_dir)
-        .include(&native_dir)
-        .file(lib_dir.join("LESS.c"))
-        .file(lib_dir.join("canonical.c"))
-        .file(lib_dir.join("codes.c"))
-        .file(lib_dir.join("fips202.c"))
-        .file(lib_dir.join("keccakf1600.c"))
-        .file(lib_dir.join("monomial.c"))
-        .file(lib_dir.join("rng.c"))
-        .file(lib_dir.join("seedtree.c"))
-        .file(lib_dir.join("sign.c"))
-        .file(lib_dir.join("sort.c"))
-        .file(lib_dir.join("transpose.c"))
-        .file(lib_dir.join("transpose_neon.c"))
-        .file(lib_dir.join("utils.c"))
-        .file(native_dir.join("less_shim.c"))
-        .define("CATEGORY", "252")
-        .define("TARGET", "45")
-        .define("USE_NEON", "1")
-        .flag_if_supported("-std=c99")
-        .flag_if_supported("-flax-vector-conversions")
-        .compile("less_neon");
-
-    println!("cargo:rustc-link-lib=m");
+    native_cc::compile_native_library(native_cc::NativeLibrarySpec {
+        vendor_subdir: "vendor/neon",
+        sources: &[
+            "LESS.c",
+            "canonical.c",
+            "codes.c",
+            "fips202.c",
+            "keccakf1600.c",
+            "monomial.c",
+            "rng.c",
+            "seedtree.c",
+            "sign.c",
+            "sort.c",
+            "transpose.c",
+            "transpose_neon.c",
+            "utils.c",
+        ],
+        native_shim: "less_shim.c",
+        defines: &[("CATEGORY", "252"), ("TARGET", "45"), ("USE_NEON", "1")],
+        flags: &["-std=c99", "-flax-vector-conversions"],
+        compile_name: "less_neon",
+        link_libs: &["m"],
+    });
 }
